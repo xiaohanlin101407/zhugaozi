06b5a5409563f1a9b759b57a4d3953342674f9e7
Xiaohanlin1014 <69285512+Xiaohanlin1014@users.noreply.github.com>
2020-08-15 20:17:40 +0800

    Add files via upload

--- 1.0.0.py
+++ 1.0.0.py
@@ -5,26 +5,30 @@
 from tkinter import *
 from tkinter.messagebox import *
 from bs4 import BeautifulSoup
+import tkinter.font as f
 import getpass,os,requests,zipfile,shutil,random,send2trash
 computeruser = "C:\\Users\\"+getpass.getuser()+"\\Desktop\\"
-zhanghao,password,dengluchuangkou,zhucechuangkou,main,guess,game1tk,number=None,None,None,None,None,None,None,None
+zhanghao,password,dengluchuangkou,zhucechuangkou,main,guess,game1tk,number=None,None,Tk(),None,None,None,None,None
+font1 = f.Font(size=30)
 version = ['1.0.0.py']
+
+
 
 #require from this
 def require():
     global computeruser,zhanghao,password,dengluchuangkou,zhucechuangkou
     try:
-        res = requests.get("https://github.com/Xiaohanlin1014/zhugaozi")
-        result = requests.get("https://github.com/Xiaohanlin1014/zhugaozi/archive/master.zip")
+        res = requests.get("https://github.com/Xiaohanlin1014/record")
+        result = requests.get("https://github.com/Xiaohanlin1014/record/archive/master.zip")
         with open(computeruser+"master.zip", "wb") as file:  
             file.write(result.content)
             file.close()
             res.close()
             result.close()
             print(computeruser+"zhugaozigames")
-            unpackedzip(computeruser+"master.zip",computeruser+"zhugaozigames",1)
+            unpackedzip(computeruser+"master.zip",computeruser,1)
     except Exception as err:
-        pass
+        print(str(err)+'in line 34')
 
 
 
@@ -37,13 +41,16 @@ def unpackedzip(address,unpackaddress,mode):
         aimzip.close()
         os.unlink(address)
     except Exception as err:
-        print(err)
+        print(str(err)+'in line 47')
     if mode == 1:
-        if len(version) < len(os.chdir(computeruser+'/zhugaozigames/zhugaozi-master')):
+        if len(version) < len(os.listdir(computeruser+'/zhugaozigames/zhugaozi-master')):
             shutil.move(computeruser+'/zhugaozigames/zhugaozi-master'+os.chdir(computeruser+'/zhugaozigames/zhugaozi-master')[-1],computeruser+'/zhugaozigames')
             send2trash.send2trash(computeruser+'/zhugaozigames/'+version[-1])
+            showinfo(title='提示',message='有新版本!请在同级目录下寻找新版本运行')
+        else:
+            start()
     elif mode == 2:
-        start()
+        mainwindow()
     else:
         print('ERROR!')
     
@@ -53,7 +60,7 @@ def unpackedzip(address,unpackaddress,mode):
 def start():
     global computeruser,zhanghao,password,dengluchuangkou,zhucechuangkou
     a=Tk()
-    temprery = os.path.isdir(computeruser+'zhugaozigames')
+    temprery = os.path.isdir(computeruser+'zhugaozigames/userdata')
     if temprery:
         a.destroy()
         denglu()
@@ -66,21 +73,22 @@ def start():
 #into the mainwindow
 def denglu():
     global computeruser,zhanghao,password,dengluchuangkou,zhucechuangkou
-    dengluchuangkou = Tk()
+    dengluchuangkou.title('登录')
     #label
-    Label(dengluchuangkou,text = '账号：').grid(row=1,column=1)
-    Label(dengluchuangkou,text = '密码：').grid(row=2,column=1)
+    Label(dengluchuangkou,text = '账号：',font = font1).grid(row=1,column=1)
+    Label(dengluchuangkou,text = '密码：',font = font1).grid(row=2,column=1)
     #entry
-    zhanghao = Entry(dengluchuangkou)
-    zhanghao.grid(row=1,column=2)
-    password = Entry(dengluchuangkou)
-    password.grid(row=2,column=2)
-    Button(dengluchuangkou,text='登录',command=bijiao).grid(row=3,column=2)
+    zhanghao = Entry(dengluchuangkou,width=50)
+    zhanghao.grid(row=1,column=2,ipady=20)
+    password = Entry(dengluchuangkou,width=50)
+    password.grid(row=2,column=2,ipady=20)
+    Button(dengluchuangkou,text='登录',command=bijiao,font = font1).grid(row=3,column=2)
     dengluchuangkou.mainloop()
 
 def zhuce():
     global computeruser,zhanghao,password,dengluchuangkou,zhucechuangkou
     zhucechuangkou = Tk()
+    zhucechuangkou.title('注册')
     #label
     Label(zhucechuangkou,text = '注册账号：').grid(row=1,column=1)
     Label(zhucechuangkou,text = '注册密码：').grid(row=2,column=1)
@@ -128,6 +136,7 @@ def xieru():
 def mainwindow():
     global computeruser,zhanghao,password,dengluchuangkou,zhucechuangkou,main
     main=Tk()
+    main.title('欢迎来到主窗口')
     try:
         maingif = PhotoImage(file = computeruser+'zhugaozigames/image-master/mainwindow.gif')
         Button(main,text='game1',command=game1).grid(row=1,column=2)
@@ -144,9 +153,8 @@ def mainwindow():
             showinfo(title='DOWNLAND',message='Downloading the file,close this and login again.')
             main.destroy()
         unpackedzip(computeruser+"master.zip",computeruser+"zhugaozigames",2)
-
     main.mainloop()
-
+#height
 def game1():
     global guess,main,game1tk,number
     main.destroy()
@@ -167,12 +175,12 @@ def belongs_game1tk():
         times = times+1
         playerguess = int(guess.get())
         if (playerguess < number):
-            Label(game1tk,text='too low!',bg='red').grid(row=3,column=1)
+            Label(game1tk,text='too low!',bg='red',font=font1,fg='white').grid(row=3,column=1)
         elif (number < playerguess):
-            Label(game1tk,text='too high',bg='green').grid(row=3,column=1)
+            Label(game1tk,text='too high',bg='green',font=font1,fg='white').grid(row=3,column=1)
         else:
-            game1tk.destroy()
             showinfo(title='提示',message='你猜中了！即将返回主窗口')
+            game1tk.destroy()
             mainwindow()
     except ValueError as err:
         print(str(err)+guess.get()+'   '+str(type(guess)))
